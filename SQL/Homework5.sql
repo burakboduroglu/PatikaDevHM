5c4a302d1d928de23f3026ceb416e485dad9e5b4
Burak Boduroğlu <80620802+burakboduroglu@users.noreply.github.com>
2023-03-19 12:51:45 +0300

    Update

---
+++ SQL/Homework5.sql
@@ -0,0 +1,10 @@
+--HOMEWORK 5
+
+--film tablosunda bulunan ve film ismi (title) 'n' karakteri ile biten en uzun (length) 5 filmi sıralayınız.
+SELECT "title", "length", "film_id" FROM film WHERE "title" LIKE '%n' ORDER BY "length" LIMIT 5
+
+--film tablosunda bulunan ve film ismi (title) 'n' karakteri ile biten en kısa (length) ikinci(6,7,8,9,10) 5 filmi(6,7,8,9,10) sıralayınız.
+SELECT "title", "length", "film_id" FROM film WHERE "title" LIKE '%n' ORDER BY "length" OFFSET 5 LIMIT 5
+
+--customer tablosunda bulunan last_name sütununa göre azalan yapılan sıralamada store_id 1 olmak koşuluyla ilk 4 veriyi sıralayınız.
+SELECT "last_name", "store_id" FROM customer WHERE store_id=1 ORDER BY "last_name" DESC LIMIT 4
