3c164e715bb1f6c7db45f5b66240a29c27f65ef9
Burak Boduroğlu <80620802+burakboduroglu@users.noreply.github.com>
2023-05-25 00:17:53 +0300

    Create index.js

---
+++ Node.js/Homework-3/index.js
@@ -0,0 +1,7 @@
+import {circleArea, circleCircumference} from './circle.js'
+
+// Area
+console.log("Area: ", circleArea(5))
+
+// Circumference
+console.log("Circumference: ", circleCircumference(5).toFixed(2));
